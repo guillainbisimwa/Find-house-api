1cb27707c179c7517fd81570749c0d1371a512b0
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-01 17:35:50 +0200

    Add model factories which povides test data

---
+++ spec/factories/favourites.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :favourite do
+    user_id nil
+  end
+end
---
+++ spec/factories/houses.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :house do
+    price { 200 }
+    details { 'Details Here' }
+    about { 'About my house' }
+    picture { 'url to pic' }
+    owner { 'Guy' }
+  end
+end
