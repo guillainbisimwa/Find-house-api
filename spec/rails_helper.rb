437fc10977795e7568becc869c4b9c128eea7a09
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-01 17:50:20 +0200

    Make the support directory autoload by default

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,3 +40,12 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+# [...]
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+# [...]
+RSpec.configuration do |config|
+  # [...]
+  config.include RequestSpecHelper, type: :request
+  # [...]
+end
